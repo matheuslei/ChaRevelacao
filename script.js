37f09f9187be87fb45a014084d0072c2eb596f4f
Matheus Leite de Castro <65515537+matheuslei@users.noreply.github.com>
2022-09-24 16:36:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
-var numeroSorteado = document.querySelector("div.box h1");
-const botaoSortear = document.querySelector("div.box button");
+var numeroSorteado = document.querySelector('div.box h1');
+const botaoSortear = document.querySelector('div.box button');
 var roleta = [];
 
-const btnOliver = document.querySelector("#btnOliver");
-const btnChloe = document.querySelector("#btnChloe");
+const btnOliver = document.querySelector('#btnOliver');
+const btnChloe = document.querySelector('#btnChloe');
 
 for (i = 1; i <= 100; i++) {
   roleta.push(i);
 }
 
-botaoSortear.addEventListener("click", () => {
-  document.getElementById("container").style.display = "none";
-  document.getElementById("loader").src = "./img/loading.svg";
-  document.getElementById("loader").style.width = "300px";
-  document.getElementById("descubra").style.display = "none";
+botaoSortear.addEventListener('click', () => {
+  document.getElementById('container').style.display = 'none';
+  document.getElementById('loader').src = './img/loading.svg';
+  document.getElementById('loader').style.width = '300px';
+  document.getElementById('descubra').style.display = 'none';
 
   setTimeout(function () {
     numeroSorteado.innerHTML =
-      "O número sorteado é " +
+      'O número sorteado é ' +
       roleta[Math.floor(Math.random() * roleta.length)];
 
-    var resultado = document.querySelector("#resultado");
+    var resultado = document.querySelector('#resultado');
 
-    resultado.style.background = "#888888";
-    resultado.style.margin = "50px";
-    resultado.style.padding = "10px";
+    resultado.style.background = '#888888';
+    resultado.style.margin = '50px';
+    resultado.style.padding = '10px';
   }, 2 * 1000);
 });
 
-btnChloe.addEventListener("click", () => {
-  document.getElementById("btnOliver").style.display = "none";
-  var btnChloe = document.querySelector("#btnChloe");
-  btnChloe.style.transition = "400ms";
-  btnChloe.style.padding = "0 50px  0 50px";
+btnChloe.addEventListener('click', () => {
+  document.getElementById('btnOliver').style.display = 'none';
+  var btnChloe = document.querySelector('#btnChloe');
+  btnChloe.style.transition = '400ms';
+  btnChloe.style.padding = '0 50px  0 50px';
+  document.querySelector('body').style.background = '#b576c7';
+  document.querySelector('body').style.transition = '400ms';
 });
-btnOliver.addEventListener("click", () => {
-  document.getElementById("btnChloe").style.display = "none";
-  var btnChloe = document.querySelector("#btnChloe");
-  btnOliver.style.transition = "400ms";
-  btnOliver.style.padding = "0 50px  0 50px";
+
+btnOliver.addEventListener('click', () => {
+  document.getElementById('btnChloe').style.display = 'none';
+  var btnOliver = document.querySelector('#btnOliver');
+  btnOliver.style.transition = '400ms';
+  btnOliver.style.padding = '0 50px  0 50px';
+  document.querySelector('body').style.background = '#3a9795';
+  document.querySelector('body').style.transition = '400ms';
 });
